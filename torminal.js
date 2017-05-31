6e2cabb23019b88766444930a93168a1fbb95848
Henry Gruett <henrikgruett@gmail.com>
2017-05-31 16:34:43 -0500

    Fixed logo url

--- torminal.js
+++ torminal.js
@@ -191,7 +191,7 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
   //
   return {
     init: function() {
-      output('<img align="left" src="https://cubeupload.png" width="100" height="100" style="padding: 0px 10px 20px 0px"><h2 style="letter-spacing: 4px">Torminal</h2><p>' + new Date() + '</p><p>Enter "help" for more information.</p>');
+      output('<img align="left" src="https://i.cubeupload.com/xYIaXA.png" width="100" height="100" style="padding: 0px 10px 20px 0px"><h2 style="letter-spacing: 4px">Torminal</h2><p>' + new Date() + '</p><p>Enter "help" for more information.</p>');
     },
     output: output
   }
