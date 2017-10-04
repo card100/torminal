dfad07b4bba995e8cd777019efee96a06224e0a3
Henry Gruett <henrikgruett@gmail.com>
2017-10-04 12:30:19 -0500

    Added about command

--- torminal.js
+++ torminal.js
@@ -135,6 +135,8 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
         case 'nano':
           output('');
           break;
+        case 'about':
+          output('<img align="left" class="undrag" src="xYIaXA.png" width="100" height="100" style="padding: 0px 10px 20px 0px"><h2 style="letter-spacing: 4px">Torminal</h2><p>' + new Date() + '</p><p>Enter "help" for more information.</p><br><p>(c) 2017 Henry Gruett Under <a href="https://card100.github.io/torminal/LICENSE">MIT License</a>');
         case 'help':
           output('<div class="ls-files">' + CMDS_.join('<br>') + '</div>');
           break;
