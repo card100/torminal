21e3ad77dc23aba37c91ce2c4a71f0cb1ff8b122
Nanomotion <zspring05@roadrunner.com>
2017-10-05 19:07:54 -0400

    Add license command

--- torminal.js
+++ torminal.js
@@ -24,7 +24,7 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
   var output_ = document.querySelector(outputContainer);
 
   const CMDS_ = [
-    'about', 'calc [-n]', 'clear', 'date', 'echo [-neE]', 'git', 'uname', 'whoami'
+    'about', 'calc [-n]', 'clear', 'date', 'echo [-neE]', 'git', 'uname', 'whoami', 'license'
   ];
 
   var fs_ = null;
@@ -157,6 +157,36 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
         case 'calc':
           output( math.eval(args.join(' ')) );
           break;
+        case 'license':
+          var txt = "\
+            <span class=\"blue\">Torminal is licensed under the <a href=\"https://github.com/card100/torminal/blob/master/LICENSE\">MIT License</a>.</span><br>\
+            <br>\
+            ----------<br>\
+            <br>\
+            MIT License<br>\
+            <br>\
+            Copyright (c) 2017 Henry Gruett (GitHub/@card100)<br>\
+            <br>\
+            Permission is hereby granted, free of charge, to any person obtaining a copy<br>\
+            of this software and associated documentation files (the \"Software\"), to deal<br>\
+            in the Software without restriction, including without limitation the rights<br>\
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell<br>\
+            copies of the Software, and to permit persons to whom the Software is<br>\
+            furnished to do so, subject to the following conditions:<br>\
+            <br>\
+            The above copyright notice and this permission notice shall be included in all<br>\
+            copies or substantial portions of the Software.<br>\
+            <br>\
+            THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR<br>\
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,<br>\
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE<br>\
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER<br>\
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,<br>\
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE<br>\
+            SOFTWARE.\
+              ";
+          output(txt)
+          break;
         default:
           if (cmd) {
             output('<span class="yellow">' + cmd + ':</span><span class="red"> command not found</span>');
@@ -206,7 +236,7 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
   //
   return {
     init: function() {
-      output('<img align="left" class="undrag" src="xYIaXA.png" width="100" height="100" style="padding: 0px 10px 20px 0px"><h2 style="letter-spacing: 4px">Torminal</h2><p>' + new Date() + '</p><p>Enter "help" for more information.</p><br><p>(c) 2017 Henry Gruett Under <a href="https://card100.github.io/torminal/LICENSE">MIT License</a>');
+      output('<img align="left" class="undrag" src="xYIaXA.png" width="100" height="100" style="padding: 0px 10px 20px 0px"><h2 style="letter-spacing: 4px">Torminal</h2><p>' + new Date() + '</p><p>Enter "help" for more information.</p><br><p>(c) 2017 Henry Gruett Under <a href="https://github.com/card100/torminal/blob/master/LICENSE">MIT License</a>. Type \'license\' for more info.');
     },
     output: output
   }
