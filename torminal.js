ea14f782005c660e79fa7ba656ea3cf249a6be59
Strudy <aravnarula18@gmail.com>
2017-10-08 16:33:28 -0400

    Update torminal.js

--- torminal.js
+++ torminal.js
@@ -196,7 +196,7 @@ var Terminal = Terminal || function(cmdLineContainer, outputContainer) {
               break;
         default:
           if (cmd) {
-            output('<span class="yellow">' + cmd + ':</span><span class="red"> command not found</span>');
+            output('<span class="yellow">' + cmd + ':</span><span class="red"> The Command Can't be Found</span>');
           }
       };
 
